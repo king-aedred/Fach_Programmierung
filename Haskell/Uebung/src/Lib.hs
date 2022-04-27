13c8f7fd01a30e991a8909ff2268764a2a4f153c
Hannes Marski <asdfglay@gmail.com>
2022-04-27 10:40:03 +0200

    added Übung 3

--- Haskell/Uebung/src/Lib.hs
+++ Haskell/Uebung/src/Lib.hs
@@ -5,6 +5,7 @@ module Lib
 someFunc :: IO ()
 someFunc = putStrLn "Hello World"
 
+-- Übung 2
 -- Aufgabe 1
 bincoeff :: Int -> Int -> Int
 bincoeff n k
@@ -75,3 +76,58 @@ ck x = maximum x
 collatz :: Int -> [Int]
 collatz 1 = [1]
 collatz n n: (collatz (cnext n))
+
+-- Übung 3
+-- Aufgabe 1
+-- a
+isPrefix :: Stirng -> String -> Bool
+isPrefix [] ys_= True
+isPrefix xs [] = False
+isPrefix (x:xs) (y:ys) = x==y && isPrefix xs ys
+
+--b
+countPattern :: String -> String -> Int
+countPattern [] [] = 1
+countPattern x [] = 0
+countPattern x (y:ys)
+    |isPrefix x (y:ys) = countPattern x ys +1
+    |otherwise countPattern x ys
+
+--Aufgabe 2
+data BinTree = Branch Int BinTree BinTree | Nil
+ deriving [Show]
+
+-- a
+mytree :: BinTree
+mytree = Branch 0
+                NIL
+                (Branch 3
+                        (Branch 1 Nil Nil)
+                        (Branch 5 Nil Nil))
+
+-- b                        
+equal :: BinTree -> BinTree -> Bool
+equal Nil Nil = True
+equal _ Nil = False
+equal Nil _ = False
+equal (Branch x lx rx) (Branch y ly ry) = x==y && equal lx ly && equal rx ry
+
+--c
+insert :: BinTree -> [Int] -> BinTree
+insert t [] = t
+insert t (x:xs) = insert (insertOne t x) xs
+
+insertOne :: BinTree -> Int -> BinTree
+insertOne Nil x = Branch x Nil Nil
+insertOne (Branch x l r) i
+    |i<=x = Branch x (insertOne l i) r
+    |otherwise = Branch x l (insertOne r i)
+
+--d
+unwind :: BinTree -> [Int]
+unwind t = hilfunwind [t]
+
+hilfunwind :: [BinTree] -> [Int]
+hilfunwind [] = []
+hilfunwind ((Branch x l r):xs) = x. hilfunwind (xs ++[l,r])
+hilfunwind (Nil:xs) = hilfunwind xs
